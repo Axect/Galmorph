3c9fe9304587286dd79f237ff520cbbfd1eae7ba
Axect <axect@outlook.kr>
2020-10-05 03:02:04 +0900

    Rust FITSIO trial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+extern crate peroxide;
+extern crate fitsio;
+use peroxide::fuga::*;
+use fitsio::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut fptr = FitsFile::open("data/nsa_v1_0_1.fits")?;
+
+    fptr.pretty_print()?;
+
+    let hdu = fptr.hdu(0)?;
+
+    if let hdu::HduInfo::ImageInfo { shape, .. } = hdu.info {
+        println!("Image is {}-dimensional", shape.len());
+        println!("Found image with shape {:?}", shape);
+    }
+
+    Ok(())
 }
